1b658a6dd6db27773c8ca451fa28dade9115681c
muntasir-fahii <fa3877957@gmail.com>
2023-11-02 01:12:55 +0600

    first commit

---
+++ app/(home)/page.tsx
@@ -0,0 +1,31 @@
+const HomePage = () => {
+  return (
+    <main>
+      <section className="container py-20">
+        <h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Exercitationem, soluta.
+        </h1>
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Exercitationem, soluta.
+        </h2>
+        <h3>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Exercitationem, soluta.
+        </h3>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Exercitationem, soluta.
+        </p>
+        <small>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Exercitationem, soluta.
+        </small>
+      </section>
+    </main>
+  );
+};
+
+export default HomePage;
