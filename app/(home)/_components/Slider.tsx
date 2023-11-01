fc01c1bf0209158a5a39a9f5663c60c6a465af55
muntasir-fahii <fa3877957@gmail.com>
2023-11-02 02:48:50 +0600

    slider done

--- app/(home)/_components/Slider.tsx
+++ app/(home)/_components/Slider.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { cn } from '@/app/lib/utils';
-
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -9,6 +8,9 @@ import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 import 'swiper/css/pagination';
 import { data } from '@/data/SliderContents';
 import Image from 'next/image';
+import Overlay from '@/components/ui/Overlay';
+import Link from 'next/link';
+import { buttonVariants } from '@/components/ui/Button';
 
 const Slider = () => {
   return (
@@ -27,7 +29,7 @@ const Slider = () => {
         className='mySwiper h-full w-full'
       >
         {data.map((slide) => (
-          <SwiperSlide key={slide.heading} className='h-full w-full'>
+          <SwiperSlide key={slide.heading} className='relative h-full w-full'>
             <Image
               src={slide.image}
               alt={slide.sub}
@@ -36,6 +38,17 @@ const Slider = () => {
               priority
               className='h-full w-full object-cover'
             />
+            <Overlay />
+            <div className='sp container absolute bottom-0 left-0 right-0 top-0 h-full w-full space-y-5 text-white'>
+              <h1>{slide.heading}</h1>
+              <p className='max-w-6xl'>{slide.sub}</p>
+              <Link
+                href='/beauty-packages'
+                className={cn(buttonVariants({ variant: 'secondary' }))}
+              >
+                Browse Beauty Packages
+              </Link>
+            </div>
           </SwiperSlide>
         ))}
       </Swiper>
